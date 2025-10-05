3e71a484a33c5e38513a2d054bd9a5274ca991a2
hickmb34@gmail.com <hickmb34@gmail.com>
2025-10-05 17:21:55 +0000

    Edited Base, About, theme/parameters.yaml and Home

--- client_code/About/__init__.py
+++ client_code/About/__init__.py
@@ -15,8 +15,7 @@ class About(AboutTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-    .body {
-      background-image: url(clouds.png)};
-    background
+   
+    
 
     # Any code you write here will run before the form opens.
