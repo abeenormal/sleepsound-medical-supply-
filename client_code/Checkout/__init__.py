0c267ed07268f434b59ccdf7a93a62e966af27c1
hickmb34@gmail.com <hickmb34@gmail.com>
2025-10-19 03:26:10 +0000

    Edited Checkout

--- client_code/Checkout/__init__.py
+++ client_code/Checkout/__init__.py
@@ -22,8 +22,8 @@ class Checkout(CheckoutTemplate):
 
   def update_form(self, id_name):
     product = anvil.server.call('get_product_details', id_name)
-    self.product = product
-    self.name_label.text = product['name']
+    self.products = product
+    self.name_label.text = product["name"]
     self.description_label.text = product['description']
     self.price_label.text = f"${product['price']} USD"
     self.image_content.source = product['image']
