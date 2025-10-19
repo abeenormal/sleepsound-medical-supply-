f289dabe32dc4d371e26730b999b6dab40b86b91
hickmb34@gmail.com <hickmb34@gmail.com>
2025-10-19 02:38:45 +0000

    Edited Products and OurProducts

--- client_code/Products.py
+++ client_code/Products.py
@@ -17,22 +17,22 @@ from anvil.tables import app_tables
 
 
 class Products(ProductsTemplate):
-  def __init__(self,id_name, add_cart_button, description,image,price,button_callback, **properties):
+  def __init__(self,id_name, button, description,image,price,button_callback, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.name_label.content = id_name
     self.description_label.content = description
     self.image_content.source = image
-    self.button.text = add_cart_button
-    self.price_label.text = price
+    self.button.text = price
     self.button_callback = button_callback
     # Any code you write here will run before the form opens.
 
-  def add_cart_button_click(self, **event_args):
+    
+  def button_click(self, **event_args):
     """This method is called when the button is clicked"""
-    self.content_panel.add_component(AddToCart('item_name', 'item_description', 'it_image', 'button_callback', 'item_price'))
-   
-   
+    self.button_callback(self.name_label.content.lower()) 
+
+
 
 
 
